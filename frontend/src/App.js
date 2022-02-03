0e472308fed08105c782c48612fe196e5261e660
Girlie Razon <77394701+girlierazon84@users.noreply.github.com>
2022-02-03 09:41:30 +0100

    ta bort onödiga filer

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import './utils/global/css/global.css';
+import Footer from "./components/footer/Footer";
+import NavigationBar from "./components/nav/NavigationBar";
+
+function App () {
+    return (
+        <>
+            <h1>
+                <NavigationBar/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <Footer/>
+            </h1>
+        </>
+    );
+}
+
+export default App;
